f0d53326ead86c8982a55bd3eb244a09e3257639
Aatu Mikkonen <t5miaa00@students.oamk.fi>
2017-11-24 12:34:40 +0200

    my edits

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+	<div className="App">
+	  <div className="App-header"><h1 className="App-title">HELLO</h1></div>
+	  <div className="App-content">
+	    <div className="pad">CONTAINER</div>
+	    <div className="main">FUCK</div>
+	    <div className="pad">ANOTER</div>
+	  </div>
+	</div>
     );
   }
 }
