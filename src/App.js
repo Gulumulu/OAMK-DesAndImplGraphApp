4f103fc2d7ba5d637802e1f095c7f23553647f6c
Gabriel Quirschfeld <xquirs00@stud.fit.vutbr.cz>
2017-11-28 10:34:34 +0200

    interface update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './components/Menu';
+import Indicator from './components/Indicators';
+import Scenario from './components/Scenarios';
+import Graphs from './components/Graphs';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
 
@@ -10,9 +13,9 @@ class App extends Component {
 	<div className="App">
 	  <div className="App-header"><h1 className="App-title">HELLO</h1></div>
 	  <div className="App-content">
-	    <div className="pad">CONTAINER</div>
-	    <div className="main">FUCK</div>
-	    <div className="pad">ANOTER</div>
+	    <div className="pad"><Scenario/></div>
+	    <div className="main"><Graphs/></div>
+	    <div className="pad"><Indicator/></div>
 	  </div>
 	</div>
     );
--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -7,9 +7,6 @@ class Graphs extends Component {
         return (
             <div className="row">
                 <div className="col-md-3" class="w1">graphs</div>
-                <div className="col-md-3" class="w2">graphs</div>
-                <div className="col-md-3" class="w3">graphs</div>
-                <div className="col-md-3" class="w4">graphs</div>
             </div>
         )
 
